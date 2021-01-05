74ff4e8b95304c92e17a39dc4fad7a0271ed3213
Sujit D'Mello <sujit@hotmail.com>
2021-01-04 20:29:11 -0500

    Added the text panel display

--- src/lighttest/teamslight.py
+++ src/lighttest/teamslight.py
@@ -7,9 +7,33 @@ from flask import Flask, json, request
 from urllib.parse import urlparse, parse_qs
 from threading import Thread
 
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
 lastAwayTime = 0
 host = "192.168.1.206"
 presenting = False
+
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+# Load default font.
+font = ImageFont.truetype('good.ttf', 16)
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
 
 def set_light_color(color):
     if color == 'RED':
@@ -40,23 +64,34 @@ def set_teams_light():
     presenting = False
     if status == 'Available':
         set_light_color('GREEN')
+        display_message('Free', 'Come on', 'down')
     if status == 'Busy':
         set_light_color('RED')
+        display_message('Busy', 'Not on a', 'call')
     if status == 'InAMeeting':
         set_light_color('RED')
+        display_message('Meeting', 'Keep quiet', 'please')
     if status == 'Presenting':
         presenting = True
+        display_message('Presenting', 'do not', 'disturb')
         set_light_color('RED')
     if status == 'OnThePhone':
         set_light_color('RED')
+        display_message('Phone', 'Keep quiet', 'please')
     if status == 'DoNotDisturb':
         set_light_color('RED')
+        display_message('Focused', 'Keep quiet', 'please')        
     if status == 'Away':
         set_light_color('YELLOW')
+        display_message('Away', 'Goofing', 'Off')        
         if lastAwayTime == 0:
             lastAwayTime = time()
     if status == 'BeRightBack':
         set_light_color('YELLOW')
+        display_message('Away', 'Be right', 'back')         
+    if status == 'Offline':
+        set_light_color('OFF')
+        display_message('Offline', 'Gone', 'Fishing!')            
     return "Set status to " + status,200
 
 def threaded_task():
@@ -101,6 +136,43 @@ def init_color_board():
     RED.start(0)
     GREEN.start(0)
     BLUE.start(0)    
+
+def display_message(message1, message2, message3):
+
+
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Write two lines of text.
+
+    draw.text((x, top),       "STATUS",  font=font, fill=255)
+    draw.text((x, top+16),     message1, font=font, fill=255)
+    draw.text((x, top+32),     message2,  font=font, fill=255)
+    draw.text((x, top+48),     message3,  font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
 
 
 init_color_board()
